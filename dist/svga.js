11a703e6a9fd86e568dbc5139f990866ddd3f6b0
Ivan Reese <ivanreese@gmail.com>
2017-09-08 18:27:16 -0600

    Add NaN checks to scope transform

--- dist/svga.js
+++ dist/svga.js
@@ -3001,6 +3001,10 @@
         },
         set: function(val) {
           if (x !== val) {
+            if (isNaN(val)) {
+              console.log(scope);
+              throw new Error("^^^ @x was set to a NaN value");
+            }
             x = val;
             return RAF(applyTransform, true, 1);
           }
@@ -3012,6 +3016,10 @@
         },
         set: function(val) {
           if (y !== val) {
+            if (isNaN(val)) {
+              console.log(scope);
+              throw new Error("^^^ @y was set to a NaN value");
+            }
             y = val;
             return RAF(applyTransform, true, 1);
           }
@@ -3023,6 +3031,10 @@
         },
         set: function(val) {
           if (rotation !== val) {
+            if (isNaN(val)) {
+              console.log(scope);
+              throw new Error("^^^ @rotation was set to a NaN value");
+            }
             rotation = val;
             return RAF(applyTransform, true, 1);
           }
@@ -3034,6 +3046,10 @@
         },
         set: function(val) {
           if (scaleX !== val || scaleY !== val) {
+            if (isNaN(val)) {
+              console.log(scope);
+              throw new Error("^^^ @scale was set to a NaN value");
+            }
             scaleX = scaleY = val;
             return RAF(applyTransform, true, 1);
           }
@@ -3045,6 +3061,10 @@
         },
         set: function(val) {
           if (scaleX !== val) {
+            if (isNaN(val)) {
+              console.log(scope);
+              throw new Error("^^^ @scaleX was set to a NaN value");
+            }
             scaleX = val;
             return RAF(applyTransform, true, 1);
           }
@@ -3056,6 +3076,10 @@
         },
         set: function(val) {
           if (scaleY !== val) {
+            if (isNaN(val)) {
+              console.log(scope);
+              throw new Error("^^^ @scaleY was set to a NaN value");
+            }
             scaleY = val;
             return RAF(applyTransform, true, 1);
           }
